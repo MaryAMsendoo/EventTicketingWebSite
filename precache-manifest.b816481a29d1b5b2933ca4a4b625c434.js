2cca94c661db1f481db5eeb59f901b2c12bfe633
MARY <marymsen05@gmail.com>
2024-08-26 18:50:42 +0100

    Updates

---
+++ precache-manifest.b816481a29d1b5b2933ca4a4b625c434.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "686bc404fd244150345129d579f9389e",
+    "url": "%3CMaryAMsendoo%3E.github.io/%3CEventTicketingWebSite%3E/index.html"
+  },
+  {
+    "revision": "13c24e01c28020664734",
+    "url": "%3CMaryAMsendoo%3E.github.io/%3CEventTicketingWebSite%3E/static/css/main.a0f804d8.chunk.css"
+  },
+  {
+    "revision": "f4ca4c8583af9260c5e9",
+    "url": "%3CMaryAMsendoo%3E.github.io/%3CEventTicketingWebSite%3E/static/js/2.ee292020.chunk.js"
+  },
+  {
+    "revision": "a70cfba45799f8a4deb5",
+    "url": "%3CMaryAMsendoo%3E.github.io/%3CEventTicketingWebSite%3E/static/js/3.cf2aeaea.chunk.js"
+  },
+  {
+    "revision": "13c24e01c28020664734",
+    "url": "%3CMaryAMsendoo%3E.github.io/%3CEventTicketingWebSite%3E/static/js/main.03c6f08a.chunk.js"
+  },
+  {
+    "revision": "c849d80c41e27ea6ba63",
+    "url": "%3CMaryAMsendoo%3E.github.io/%3CEventTicketingWebSite%3E/static/js/runtime~main.8b6be1e1.js"
+  },
+  {
+    "revision": "48871cd35eda4f62d76d5a75aa8978be",
+    "url": "%3CMaryAMsendoo%3E.github.io/%3CEventTicketingWebSite%3E/static/media/Outer-image.48871cd3.svg"
+  },
+  {
+    "revision": "fc1f97e3f76b1fd6865c4ee947fcd83a",
+    "url": "%3CMaryAMsendoo%3E.github.io/%3CEventTicketingWebSite%3E/static/media/Rectangle 12 (1).fc1f97e3.png"
+  },
+  {
+    "revision": "c5ca64947dc204936b368c4d203be27f",
+    "url": "%3CMaryAMsendoo%3E.github.io/%3CEventTicketingWebSite%3E/static/media/Rectangle 12 (2).c5ca6494.png"
+  },
+  {
+    "revision": "9ccf8b8253d5ea9c0a88a560a572e9fc",
+    "url": "%3CMaryAMsendoo%3E.github.io/%3CEventTicketingWebSite%3E/static/media/Rectangle 12 (3).9ccf8b82.png"
+  },
+  {
+    "revision": "97d05be6b8b6a4fc00663860a2554ee7",
+    "url": "%3CMaryAMsendoo%3E.github.io/%3CEventTicketingWebSite%3E/static/media/Rectangle 12 (4).97d05be6.png"
+  },
+  {
+    "revision": "162bc1b083f54f52f3acd30aab1df94a",
+    "url": "%3CMaryAMsendoo%3E.github.io/%3CEventTicketingWebSite%3E/static/media/Rectangle 12 (5).162bc1b0.png"
+  },
+  {
+    "revision": "451a2f0d5f9bcf2bea228aefeb9e11bd",
+    "url": "%3CMaryAMsendoo%3E.github.io/%3CEventTicketingWebSite%3E/static/media/Rectangle 12.451a2f0d.svg"
+  },
+  {
+    "revision": "d92b7b18e69a8e0bc0019db444794027",
+    "url": "%3CMaryAMsendoo%3E.github.io/%3CEventTicketingWebSite%3E/static/media/Rectangle 14 (1).d92b7b18.png"
+  },
+  {
+    "revision": "e7e08ce2505d4002b7698a0319aac208",
+    "url": "%3CMaryAMsendoo%3E.github.io/%3CEventTicketingWebSite%3E/static/media/Rectangle 14 (2).e7e08ce2.png"
+  },
+  {
+    "revision": "32bc3ed2a38fd95af1958ff8da3d6c5b",
+    "url": "%3CMaryAMsendoo%3E.github.io/%3CEventTicketingWebSite%3E/static/media/Rectangle 14.32bc3ed2.png"
+  },
+  {
+    "revision": "bd8bd4c8bb944489001066d9f2f5cb39",
+    "url": "%3CMaryAMsendoo%3E.github.io/%3CEventTicketingWebSite%3E/static/media/bg_image.bd8bd4c8.svg"
+  },
+  {
+    "revision": "56c8d80832e37783f12c05db7c8849e2",
+    "url": "%3CMaryAMsendoo%3E.github.io/%3CEventTicketingWebSite%3E/static/media/fa-brands-400.56c8d808.ttf"
+  },
+  {
+    "revision": "715d593456fa02fe72a008a72398f5be",
+    "url": "%3CMaryAMsendoo%3E.github.io/%3CEventTicketingWebSite%3E/static/media/fa-brands-400.715d5934.woff2"
+  },
+  {
+    "revision": "2d89b49ac28614e9ccd9c81308b5120c",
+    "url": "%3CMaryAMsendoo%3E.github.io/%3CEventTicketingWebSite%3E/static/media/fa-regular-400.2d89b49a.woff2"
+  },
+  {
+    "revision": "370dd5af19f8364907b6e2c41f45dbbf",
+    "url": "%3CMaryAMsendoo%3E.github.io/%3CEventTicketingWebSite%3E/static/media/fa-regular-400.370dd5af.ttf"
+  },
+  {
+    "revision": "237f4a0afbdb652fb2330ee7e1567dd3",
+    "url": "%3CMaryAMsendoo%3E.github.io/%3CEventTicketingWebSite%3E/static/media/fa-solid-900.237f4a0a.woff2"
+  },
+  {
+    "revision": "adec7d6f310bc577f05e8fe06a5daccf",
+    "url": "%3CMaryAMsendoo%3E.github.io/%3CEventTicketingWebSite%3E/static/media/fa-solid-900.adec7d6f.ttf"
+  },
+  {
+    "revision": "38fec6307db2547752379b51322ded10",
+    "url": "%3CMaryAMsendoo%3E.github.io/%3CEventTicketingWebSite%3E/static/media/fa-v4compatibility.38fec630.ttf"
+  },
+  {
+    "revision": "7c428138d15b872f193c40828f6fa0d3",
+    "url": "%3CMaryAMsendoo%3E.github.io/%3CEventTicketingWebSite%3E/static/media/fa-v4compatibility.7c428138.woff2"
+  },
+  {
+    "revision": "310545f962d64a1b840553975fc5fe44",
+    "url": "%3CMaryAMsendoo%3E.github.io/%3CEventTicketingWebSite%3E/static/media/image 3.310545f9.png"
+  },
+  {
+    "revision": "bdf3412e7ff0c91320035a7e8771ad24",
+    "url": "%3CMaryAMsendoo%3E.github.io/%3CEventTicketingWebSite%3E/static/media/ticket-2 1.bdf3412e.svg"
+  }
+]);
